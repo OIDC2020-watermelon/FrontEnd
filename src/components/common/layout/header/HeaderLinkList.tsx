5b4b59c045b95ae0d88bf97c60f0e96feb4985e5
jodmsoluth <jodmsoluth@gmail.com>
2020-07-31 10:42:01 +0900

    feat: ⚡로그인 모달 레이아웃

--- src/components/common/layout/header/HeaderLinkList.tsx
+++ src/components/common/layout/header/HeaderLinkList.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import Modal from '../../modal/Modal';
+import Login from '../../layout/Login';
 
 export default function HeaderLinkList() {
+  const [visible, setVisible] = useState(false);
   return (
     <>
       <S.LinkListWrap>
         <Link to="/">
           <S.LinkItem>메인</S.LinkItem>
         </Link>
-        <Link to="/login">
+        <S.LinkLogin onClick={() => setVisible(!visible)}>
           <S.LinkItem>로그인</S.LinkItem>
-        </Link>
+        </S.LinkLogin>
         <Link to="/">
           <S.LinkItem>회원가입</S.LinkItem>
         </Link>
@@ -19,6 +22,7 @@ export default function HeaderLinkList() {
           <S.LinkItem>내정보</S.LinkItem>
         </Link>
       </S.LinkListWrap>
+      <Modal visible={visible} setVisible={setVisible} render={<Login />} />
     </>
   );
 }
@@ -34,4 +38,13 @@ S.LinkListWrap = styled.ul`
 
 S.LinkItem = styled.li`
   padding-left: 0.5rem;
+`;
+
+S.LinkLogin = styled.div`
+  &:hover {
+    color: #1890ff;
+    cursor: pointer;
+    transition: color 0.3s;
+    font-family: 'IBM Plex Sans', sans-serif;
+  }
 `;
--- src/pages/Login.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Login from '../components/common/layout/Login';
-export default function MainPage() {
-  return (
-    <>
-      <Login />
-    </>
-  );
-}
