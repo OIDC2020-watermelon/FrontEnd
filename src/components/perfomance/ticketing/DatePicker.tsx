8d2f5398c966e74e8092d44470dc30056200966b
jodmsoluth <jodmsoluth@gmail.com>
2020-08-17 21:55:38 +0900

    chore : :package: date picker

--- src/components/perfomance/ticketing/DatePicker.tsx
+++ src/components/perfomance/ticketing/DatePicker.tsx
@@ -26,8 +26,12 @@ export default function DatePicker({
   }
 
   function disabledDate(date: Moment) {
-    let start = ableStartDate || '2020-07-10';
-    let end = ableEndDate || '2021-07-20';
+    let start =
+      // ableStartDate ||
+      '2020-07-10';
+    let end =
+      // ableEndDate||
+      '2021-07-20';
     const now = moment(Date.now()).subtract(1, 'days');
     if (now > date) return true;
     if (date < moment(start)) {
